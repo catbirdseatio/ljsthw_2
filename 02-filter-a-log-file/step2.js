afa0f10b85db041ae0430cf93eb52a731dae56a1
Leigh Michael Forrest <leigh@catbirdseat.io>
2022-12-21 17:44:59 -0500

    decent attempt at filtering a log

--- 02-filter-a-log-file/step2.js
+++ 02-filter-a-log-file/step2.js
@@ -1,8 +1,22 @@
 import fs from "fs";
 
 const raw_log = fs.readFileSync(process.argv[2]);
+const filter_pattern = new RegExp(process.argv[3], "g")
 
-const lines = raw_log.toString().split("\n");
+let lines = raw_log
+  .toString()
+  .split("\n")
+  .map((line) => {
+    return line
+      .split(/[-"]/gi)
+      .map((element) => element.trim().replace(/["\[\]]/gi, ""))
+      .filter((element) => element.trim() !== "");
+  })
+  .filter((item) => item.length !== 0);
 
 // now you have to figure out how to split each line of the
 // web log into something meaningful
+console.log(lines.forEach(line => {
+    const match = line.find(value => filter_pattern.test(value));
+    if (match) console.log(` - - [${line[0]}] "${line[1]}" ""${line[2]}" ${line[3]} ${line[4]} "${line[5]} ${line[6]}" -`);
+}));
