c884c71c51377c1fa57b5a7ccd8054d7a1fea1fe
Leigh Michael Forrest <leigh@catbirdseat.io>
2022-12-21 17:45:26 -0500

    small tweaks

--- 02-filter-a-log-file/step2.js
+++ 02-filter-a-log-file/step2.js
@@ -14,8 +14,7 @@ let lines = raw_log
   })
   .filter((item) => item.length !== 0);
 
-// now you have to figure out how to split each line of the
-// web log into something meaningful
+
 console.log(lines.forEach(line => {
     const match = line.find(value => filter_pattern.test(value));
     if (match) console.log(` - - [${line[0]}] "${line[1]}" ""${line[2]}" ${line[3]} ${line[4]} "${line[5]} ${line[6]}" -`);
